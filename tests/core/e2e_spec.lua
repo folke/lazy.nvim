cb87aa38935d679352c585da4ac8c04bdacb33f7
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-20 08:56:59 +0100

    ci: run tests on linux only for nw

---
+++ tests/core/e2e_spec.lua
@@ -0,0 +1,35 @@
+describe("lazy", function()
+  before_each(function()
+    vim.g.lazy_did_setup = false
+    vim.go.loadplugins = true
+    for modname in pairs(package.loaded) do
+      if modname:find("lazy") == 1 then
+        package.loaded[modname] = nil
+      end
+    end
+  end)
+
+  local root = ".tests/data/nvim/lazy"
+
+  it("installs plugins", function()
+    local Lazy = require("lazy")
+    local Config = require("lazy.core.config")
+
+    local neodev = false
+    Lazy.setup({
+      {
+        "folke/neodev.nvim",
+        config = function()
+          neodev = true
+        end,
+      },
+      "folke/paint.nvim",
+    }, { install_missing = true, defaults = { lazy = true } })
+    assert(3 == vim.tbl_count(Config.plugins))
+    assert(vim.loop.fs_stat(root .. "/paint.nvim/README.md"))
+    assert(vim.loop.fs_stat(root .. "/neodev.nvim/README.md"))
+    assert(not neodev)
+    assert(Config.plugins["neodev.nvim"]._.installed)
+    assert(not Config.plugins["neodev.nvim"]._.is_local)
+  end)
+end)
