e9d3a73bbceaac0dafacd6a3c6c76ab37799d15b
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-20 14:13:52 +0100

    fix: default logs are now since 3 days ago to be in line with the docs

--- lua/lazy/core/config.lua
+++ lua/lazy/core/config.lua
@@ -15,7 +15,7 @@ M.defaults = {
   git = {
     -- defaults for the `Lazy log` command
     -- log = { "-10" }, -- show the last 10 commits
-    log = { "--since=1 days ago" }, -- show commits from the last 3 days
+    log = { "--since=3 days ago" }, -- show commits from the last 3 days
     timeout = 120, -- kill processes that take more than 2 minutes
     url_format = "https://github.com/%s.git",
   },
