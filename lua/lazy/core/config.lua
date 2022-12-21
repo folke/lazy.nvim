58f0876e81881c487ea10e393fa828a1c45c74f4
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-21 20:09:12 +0100

    fix: removed spell again from site. not needed. can download in config/spell

--- lua/lazy/core/config.lua
+++ lua/lazy/core/config.lua
@@ -142,7 +142,6 @@ function M.setup(spec, opts)
       vim.fn.fnamemodify(vim.v.progpath, ":p:h:h") .. "/lib/nvim",
       vim.fn.stdpath("config"),
       vim.fn.stdpath("config") .. "/after",
-      vim.fn.stdpath("data") .. "/site",
     }
   end
   for _, path in ipairs(M.options.performance.rtp.paths) do
