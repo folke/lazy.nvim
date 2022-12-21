2ab651864f30022751252e66b4cd2c1e36800d06
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-21 19:01:58 +0100

    fix(keys): feedkeys should include pending keys. Fixes #71

--- lua/lazy/core/handler/keys.lua
+++ lua/lazy/core/handler/keys.lua
@@ -10,7 +10,15 @@ function M:_add(keys)
     vim.keymap.del("n", keys)
     Util.track({ keys = keys })
     Loader.load(self.active[keys], { keys = keys })
-    local feed = vim.api.nvim_replace_termcodes(keys, true, false, true)
+    local extra = ""
+    while true do
+      local c = vim.fn.getchar(0)
+      if c == 0 then
+        break
+      end
+      extra = extra .. vim.fn.nr2char(c)
+    end
+    local feed = vim.api.nvim_replace_termcodes(keys .. extra, true, true, true)
     vim.api.nvim_feedkeys(feed, "m", false)
     Util.track()
   end, { silent = true })
