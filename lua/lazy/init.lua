b0582d9891525c0ade3d935c5aed33d44035eb36
williamhsieh <wh31110@gmail.com>
2022-12-31 01:52:32 +0800

    fix: duplicate state check in bootstrap

--- lua/lazy/init.lua
+++ lua/lazy/init.lua
@@ -68,10 +68,8 @@ end
 function M.bootstrap()
   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
   if not vim.loop.fs_stat(lazypath) then
-    if not vim.loop.fs_stat(lazypath) then
-      vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
-      vim.fn.system({ "git", "-C", lazypath, "checkout", "tags/stable" }) -- last stable release
-    end
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
+    vim.fn.system({ "git", "-C", lazypath, "checkout", "tags/stable" }) -- last stable release
   end
   vim.opt.rtp:prepend(lazypath)
 end
