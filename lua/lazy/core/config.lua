d7d5842d1c9a566d480db8b4a5aaf00054b99bb5
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-22 11:04:17 +0200

    fix(config): on windows default concurrency is now set to 2*available parallelism. See #887

--- lua/lazy/core/config.lua
+++ lua/lazy/core/config.lua
@@ -17,7 +17,7 @@ M.defaults = {
   -- leave nil when passing the spec as the first argument to setup()
   spec = nil, ---@type LazySpec
   lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json", -- lockfile generated after running update.
-  concurrency = nil, ---@type number limit the maximum amount of concurrent tasks
+  concurrency = jit.os:find("Windows") and (vim.loop.available_parallelism() * 2) or nil, ---@type number limit the maximum amount of concurrent tasks
   git = {
     -- defaults for the `Lazy log` command
     -- log = { "-10" }, -- show the last 10 commits
