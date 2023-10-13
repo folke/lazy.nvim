37694611946387dc79d546bdc193bc8611ac1c6d
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-13 11:37:38 +0200

    fix(float): disable swapfile for files shown in Float

--- lua/lazy/view/float.lua
+++ lua/lazy/view/float.lua
@@ -104,6 +104,8 @@ function M:mount()
     self.buf = self.buf
   elseif self.opts.file then
     self.buf = vim.fn.bufadd(self.opts.file)
+    vim.bo[self.buf].readonly = true
+    vim.bo[self.buf].swapfile = false
     vim.fn.bufload(self.buf)
     vim.bo[self.buf].modifiable = false
   elseif self.opts.buf then
