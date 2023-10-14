3271e61fe93d509f23957873032a5035d3b2c67b
ala <marzoukala2018@gmail.com>
2023-10-14 16:05:20 +0100

    update git.log in lazy.core.config

--- lua/lazy/core/config.lua
+++ lua/lazy/core/config.lua
@@ -20,8 +20,8 @@ M.defaults = {
   concurrency = jit.os:find("Windows") and (vim.loop.available_parallelism() * 2) or nil, ---@type number limit the maximum amount of concurrent tasks
   git = {
     -- defaults for the `Lazy log` command
-    -- log = { "-10" }, -- show the last 10 commits
-    log = { "-8" }, -- show commits from the last 3 days
+    -- log = { "--since=3 days ago" }, -- show commits from the last 3 days
+    log = { "-8" }, -- show the last 8 commits
     timeout = 120, -- kill processes that take more than 2 minutes
     url_format = "https://github.com/%s.git",
     -- lazy.nvim requires git >=2.19.0. If you really want to use lazy with an older version,
