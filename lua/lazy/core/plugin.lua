c8ea4122704f114a58a97660410474c171b0e80c
atusy <30277794+atusy@users.noreply.github.com>
2023-10-30 13:19:55 +0900

    feat: support dev.path as function
    In some case, `dev.path .. plugin.name` is not enoguh.
    
    For example, when using `ghq` to manage projects, plugin directories may
    vary by onewrs of the plugins.
    
    With this change, users can do something like below
    
    ``` lua
    require("lazy").setup("plugins", {
      dev = {
        path = function(p)
          -- ghq
          local path, cnt = string.gsub(p.url, "^https://(.*)%.git$", "~/ghq/%1")
          if cnt == 1 then
            return path
          end
    
          -- fallback to default
          return "~/projects/" .. plugin.name
        end,
      },
    })
    ```

--- lua/lazy/core/plugin.lua
+++ lua/lazy/core/plugin.lua
@@ -106,11 +106,16 @@ function Spec:add(plugin, results)
   end
 
   -- dev plugins
-  if
-    plugin.dev
-    and (not Config.options.dev.fallback or vim.fn.isdirectory(Config.options.dev.path .. "/" .. plugin.name) == 1)
-  then
-    dir = Config.options.dev.path .. "/" .. plugin.name
+  if plugin.dev then
+    local dir_dev
+    if type(Config.options.dev.path) == "string" then
+      dir_dev = Config.options.dev.path .. "/" .. plugin.name
+    else
+      dir_dev = Util.norm(Config.options.dev.path(plugin))
+    end
+    if not Config.options.dev.fallback or vim.fn.isdirectory(dir_dev) == 1 then
+      dir = dir_dev
+    end
   elseif plugin.dev == false then
     -- explicitely select the default path
     dir = Config.options.root .. "/" .. plugin.name
