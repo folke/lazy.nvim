a836600573fbcf1879b850d40b8cdea59b33f8da
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-03-27 09:23:15 +0100

    docs: make bootstrap work on stable and nightly. Fixes #1391

--- lua/lazy/init.lua
+++ lua/lazy/init.lua
@@ -122,7 +122,7 @@ end
 
 function M.bootstrap()
   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-  if not vim.uv.fs_stat(lazypath) then
+  if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({
       "git",
       "clone",
