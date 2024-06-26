707e2e923b341d280f6cc4546715c37fb04da9cb
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-26 11:11:11 +0200

    test: fix init test

--- tests/core/init_spec.lua
+++ tests/core/init_spec.lua
@@ -1,7 +1,11 @@
+---@module 'luassert'
+local Util = require("lazy.core.util")
+
 describe("init", function()
   it("has correct environment for tests", function()
-    for _, path in ipairs({ "config", "data", "cache", "state" }) do
-      assert(vim.fn.stdpath(path):find(".tests/" .. path))
+    for _, name in ipairs({ "config", "data", "cache", "state" }) do
+      local path = Util.norm(vim.fn.stdpath(name) --[[@as string]])
+      assert(path:find(".tests/" .. name, 1, true), path .. " not in .tests")
     end
   end)
 end)
