ba1a9c5392915c306cb12ed0b64b9b3e6372a1d5
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-29 07:12:16 +0200

    ci: bootstrap script

---
+++ bootstrap.lua
@@ -0,0 +1,48 @@
+-- Lay Bootstrapper
+-- Usage:
+-- ```lua
+-- load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
+-- ```
+local M = {}
+
+function M.setup()
+  if vim.env.LAZY_STDPATH then
+    local root = vim.fn.fnamemodify(vim.env.LAZY_STDPATH, ":p")
+    for _, name in ipairs({ "config", "data", "state", "cache" }) do
+      vim.env[("XDG_%s_HOME"):format(name:upper())] = root .. "/" .. name
+    end
+  end
+
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.api.nvim_echo({
+      {
+        "Cloning lazy.nvim\n\n",
+        "DiagnosticInfo",
+      },
+    }, true, {})
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local ok, out =
+      pcall(vim.fn.system, { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if not ok or vim.v.shell_error ~= 0 then
+      vim.api.nvim_echo({
+        {
+          "Failed to clone lazy.nvim\n",
+          "ErrorMsg",
+        },
+        {
+          vim.trim(out or ""),
+          "WarningMsg",
+        },
+        { "\nPress any key to exit", "MoreMsg" },
+      }, true, {})
+
+      vim.fn.getchar()
+      vim.cmd([[quit]])
+    end
+  end
+  vim.opt.rtp:prepend(lazypath)
+end
+M.setup()
+
+return M
