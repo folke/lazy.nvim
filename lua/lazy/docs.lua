25749704e442b5c8e2db49a6fe11260760c73d5f
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-20 09:11:45 +0100

    docs: docgen

--- lua/lazy/docs.lua
+++ lua/lazy/docs.lua
@@ -131,7 +131,7 @@ function M.colors(opts)
     { "---", "---", "---" },
   }
   Util.foreach(require(opts.modname).colors, function(group, link)
-    link = type(link) == "table" and vim.inspect(link) or link
+    link = type(link) == "table" and vim.inspect(link):gsub("%s+", " ") or link
     lines[#lines + 1] = { "**" .. opts.name .. group .. "**", "***" .. link .. "***", comments[group] or "" }
   end)
   return { content = M.table(lines) }
