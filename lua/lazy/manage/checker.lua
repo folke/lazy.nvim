715c76c40150b744e1b14de173fe287356ff428a
Erik Larkö <eriklarko@users.noreply.github.com>
2026-01-13 12:56:26 -0500

    Update notification message for available plugins

--- lua/lazy/manage/checker.lua
+++ lua/lazy/manage/checker.lua
@@ -89,8 +89,10 @@ function M.report(notify)
     end
   end
   if #lines > 0 and notify and Config.options.checker.notify and not Config.headless() then
-    table.insert(lines, 1, "# Available Plugin Updates")
+    table.insert(lines, 1, "# Plugin Updates Available")
     Util.info(lines)
+    table.insert(lines, "")
+    table.insert(lines, "Run `:Lazy update` to install")
   end
 end
 
